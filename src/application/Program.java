07e2b19a37fd2ea8e965aba89f4925fe46ac8f3d
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2019-02-25 11:08:54 -0300

    Implementing rook and possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,7 @@ import chess.ChessPosition;
 
 public class Program {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -22,7 +23,10 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);				
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -22,7 +22,7 @@ public class Rook extends ChessPiece{
 
 		Position p = new Position(0, 0);
 		// above piece
-		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		p.setValues(position.getRow() - 1, position.getColumn());
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow() - 1);
